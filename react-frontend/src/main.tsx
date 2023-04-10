f8263f53ac8414423bee1a2fad01ff1c3cb70321
josealarcontech <josealarcontech@gmail.com>
2023-04-10 14:45:12 -0600

    Update main.tsx

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
-
 import store from './store'
 import { Provider } from 'react-redux'
 import '@fontsource/roboto/300.css';
@@ -15,9 +14,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-
-      <App />
-        
+        <App />
       </Router>
     </Provider>
   </React.StrictMode>,
